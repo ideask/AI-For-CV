98fe502b1596d5ad05526b206df9df240b762d73
ideask <9982733@qq.com>
2019-07-21 22:25:40 +0800

    week6_9:modify the .sh :add the test process

--- week6_9_TinyProj/tiny_caffe_project/start.sh
+++ week6_9_TinyProj/tiny_caffe_project/start.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
+#export CUDA_VISIBLE_DEVICES=1
 set -e
+STATE=train
 
-BACKBONE=solver
 
 CAFFE_ROOT=F:/caffe/scripts/build/tools/Release
 SOLVER_ROOT=F:/Tiny_project/tiny_caffe_project
 
 TRAIN=${CAFFE_ROOT}/caffe.exe
-SOLVER=${SOLVER_ROOT}/brand_${BACKBONE}.prototxt
-LOG=${SOLVER_ROOT}/${BACKBONE}_log/${BACKBONE}_log.log
-
+SOLVER=${SOLVER_ROOT}/brand_solver.prototxt
+TESTER=${SOLVER_ROOT}/brand_network.prototxt
+LOG=${SOLVER_ROOT}/network_log/$STATE.log
+WEIGH=${SOLVER_ROOT}/models/_iter_1000.caffemodel
 echo ${TRAIN}
 echo ${SOLVER}
 echo ${LOG}
-
-${TRAIN} train --solver=${SOLVER}| tee ${LOG}
+if $STATE == train; then
+  ${TRAIN} $STATE --solver=${SOLVER} --gpu=0 2>&1 | tee $LOG
+else
+  ${TRAIN} $STATE --model=${TESTER} --weights=${WEIGH} --iterations=100 --gpu=0 2>&1 | tee $LOG
+fi
